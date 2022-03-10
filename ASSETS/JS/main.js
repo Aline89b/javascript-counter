4a477d87b57b1928038e542761411bc58c0009af
Aline89b <95961350+Aline89b@users.noreply.github.com>
2022-03-10 09:21:26 +0100

    btns created by js

--- ASSETS/JS/main.js
+++ ASSETS/JS/main.js
@@ -1,11 +1,23 @@
 
 let count = 0
-let countEl = document.querySelector("#count-el")
-let increaseEl = document.querySelector("#btn-increase")
-let decreaseEl = document.querySelector("#btn-decrease")
-let resetEl = document.querySelector("#btn-reset")
+const incrementBtn = document.createElement("button");
+incrementBtn.textContent = "Increase";
+const decrementBtn = document.createElement("button");
+decrementBtn.textContent = "Decrease";
+const resetBtn = document.createElement("button");
+resetBtn.textContent = "Reset";
+const counter = document.querySelector(".counter");
+counter.append(incrementBtn, decrementBtn, resetBtn);
 
-increaseEl.addEventListener('click', increment)
+
+resetBtn.setAttribute("class", "button");
+decrementBtn.setAttribute("class", "button");
+incrementBtn.setAttribute("class", "button");
+
+let countEl = document.querySelector("#count-el")
+
+
+incrementBtn.addEventListener('click', increment)
 
 function increment() {
  if (count >=0 && count < 100) {
@@ -14,7 +26,7 @@ function increment() {
   }
 }
 
-decreaseEl.addEventListener('click', decrement)
+decrementBtn.addEventListener('click', decrement)
 
 function decrement() {
  if (count > 0 && count <= 100) {
@@ -23,7 +35,7 @@ function decrement() {
 }
 
 
-resetEl.addEventListener("click", reset)
+resetBtn.addEventListener("click", reset)
 
 function reset() {
   countEl.innerHTML = 0
